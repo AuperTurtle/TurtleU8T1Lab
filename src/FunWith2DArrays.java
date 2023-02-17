c58bad81486b8cdbdbdfc65db4765d7cf825d6da
1E10 Laptop <sample@bths.edu>
2023-02-17 10:36:48 -0500

    .

--- src/FunWith2DArrays.java
+++ src/FunWith2DArrays.java
@@ -26,4 +26,65 @@ public class FunWith2DArrays {
         wordList[0] = wordList[maxRow];
         wordList[maxRow] = temp;
     }
+
+    public static void asciiPrinter(String[][] stuff) {
+        for (int i = 0; i < stuff.length; i++) {
+            for (int z = 0; z < stuff[i].length; z++) {
+                System.out.print(stuff[i][z]);
+            }
+            System.out.println("");
+        }
+    }
+
+    public static double average(int[][] intLists) {
+        double average = 0;
+        int counter = 0;
+        for (int i = 0; i < intLists.length; i++) {
+            for (int z = 0; z < intLists[i].length; z++) {
+                average += intLists[i][z];
+                counter++;
+            }
+        }
+        return average / counter;
+    }
+
+    public static int[] indexFound(String[][] wordList, String find) {
+        int index[] = {-1, -1};
+        for (int i = 0; i < wordList.length; i++) {
+            for (int z = 0; z < wordList[i].length; z++) {
+                if (wordList[i][z].equals(find)) {
+                    index[0] = i;
+                    index[1] = z;
+                    return index;
+                }
+            }
+        }
+        return index;
+    }
+
+    public static int[][] split(int[][] intList, int row, int column) {
+        if (row >= intList.length || column >= intList[0].length) {
+            return new int[0][0];
+        }
+        int[][] temp = new int[row + 1][column + 1];
+        if (row < intList.length && column < intList[0].length) {
+            for (int i = 0; i <= row; i++) {
+                for (int z = 0; z <= column; z++) {
+                    temp[i][z] = intList[i][z];
+                }
+            }
+        }
+            return temp;
+    }
+
+    public static int[][] invert(int[][] intLists) {
+        int[][] newList = new int[intLists[0].length][intLists.length];
+        for (int i = 0; i < newList.length; i++) {
+            for (int z = 0; z < newList[i].length; z++) {
+                newList[i][z] = intLists[z][i];
+            }
+        }
+        return newList;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,149 +1,46 @@
 import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-
-//        String[][] studentList = {
-//                {"Abby", "Don", "George", "Kim"},
-//                {"Brian", "Elenor", "Harry", "Lenny"},
-//                {"Cathy", "Fred", "Jill", "Matt"}
-//        };
-//
-//        for (int i = 0; i < studentList.length; i++) {
-//            System.out.println(Arrays.toString(studentList[i]));
-//        }
-//        System.out.println("");
-//
-//        studentList[1][2] = "Paul";
-//        for (int i = 0; i < studentList.length; i++) {
-//            System.out.println(Arrays.toString(studentList[i]));
-//        }
-//        System.out.println("");
-//
-//        String temp = studentList[0][0];
-//        studentList[0][0] = studentList[2][3];
-//        studentList[2][3] = temp;
-//        for (int i = 0; i < studentList.length; i++) {
-//            System.out.println(Arrays.toString(studentList[i]));
-//        }
-//        System.out.println("");
-//
-//        String[] temp2 = studentList[0];
-//        studentList[0] = studentList[1];
-//        studentList[1] = temp2;
-//        for (int i = 0; i < studentList.length; i++) {
-//            System.out.println(Arrays.toString(studentList[i]));
-//        }
-//        System.out.println("");
-//
-//        System.out.println(studentList[0][2] + studentList[2][0]);
-//
-//        int arr1[][] = new int[2][3];
-//        int arr2[][] = new int[3][2];
-//        for (int i = 0; i < arr1.length; i++) {
-//            System.out.println(Arrays.toString(arr1[i]));
-//        }
-//        System.out.println("");
-//
-//        for (int i = 0; i < arr2.length; i++) {
-//            System.out.println(Arrays.toString(arr2[i]));
-//        }
-//        System.out.println("");
-//
-//        arr1[0][0] = 1;
-//        arr1[0][1] = 2;
-//        arr1[0][2] = 3;
-//        arr1[1][0] = 4;
-//        arr1[1][1] = 5;
-//        arr1[1][2] = 6;
-//
-//        arr2[0][0] = 1;
-//        arr2[1][0] = 2;
-//        arr2[2][0] = 3;
-//        arr2[0][1] = 4;
-//        arr2[1][1] = 5;
-//        arr2[2][1] = 6;
-//
-//        for (int i = 0; i < arr1.length; i++) {
-//            System.out.println(Arrays.toString(arr1[i]));
-//        }
-//        System.out.println("");
-//
-//        for (int i = 0; i < arr2.length; i++) {
-//            System.out.println(Arrays.toString(arr2[i]));
-//        }
-//        System.out.println("");
-//
-//        System.out.println(arr1[0][2] + arr2[2][0]);
-
-        // TEST CODE FOR swapFrontAndBackRows
-        System.out.println("-------TEST 1-------");
-        String[][] w1 = {{"hi", "bye", "stuff", "go"},
-                {"time", "up", "you", "good"},
-                {"map", "low", "bow", "mom"}};
-        System.out.println("--Original:");
-        for (String[] innerArr : w1) {
-            System.out.println(Arrays.toString(innerArr));
+        // TEST CODE FOR invert
+        System.out.println("---invert test1---");
+        int[][] test1 = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+        int[][] ts1 = FunWith2DArrays.invert(test1);
+        for (int[] row : ts1) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
         }
-        System.out.println("--Modified:");
-        FunWith2DArrays.swapFrontAndBackRows(w1);
-        for (String[] innerArr : w1) {
-            System.out.println(Arrays.toString(innerArr));
+        System.out.println("orig array not modified:");
+        for (int[] row : test1) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
         }
-        System.out.println("-------TEST 2-------");
-        String[][] w2 = {{"time", "up", "bye"},
-                {"hi", "hit", "up"},
-                {"map", "bam", "low"},
-                {"bow", "mom", "joy"}};
-        System.out.println("--Original:");
-        for (String[] innerArr : w2) {
-            System.out.println(Arrays.toString(innerArr));
+        System.out.println("---invert test2---");
+        int[][] test2 = {
+                {1, 2, 3, 4, 5, 6},
+                {7, 8, 9, 10, 11, 12},
+                {13, 14, 15, 16, 17, 18},
+                {19, 20, 21, 22, 23, 24}
+        };
+        int[][] ts2 = FunWith2DArrays.invert(test2);
+        for (int[] row : ts2) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
         }
-        System.out.println("--Modified:");
-        FunWith2DArrays.swapFrontAndBackRows(w2);
-        for (String[] innerArr : w2) {
-            System.out.println(Arrays.toString(innerArr));
+        System.out.println("---invert test3---");
+        int[][] test3 = {{10}};
+        int[][] ts3 = FunWith2DArrays.invert(test3);
+        for (int[] row : ts3) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
         }
-
-        System.out.println("-------TEST 3-------");
-        String[][] w3 = {{"phone", "mouse", "keyboard"}};
-        System.out.println("--Original:");
-        for (String[] innerArr : w3) {
-            System.out.println(Arrays.toString(innerArr));
-        }
-        System.out.println("--Modified:");
-        FunWith2DArrays.swapFrontAndBackRows(w3);
-        for (String[] innerArr : w3) {
-            System.out.println(Arrays.toString(innerArr));
-        }
-
-        System.out.println("-------TEST 4-------");
-        String[][] w4 = {{"cat"},
-                {"dog"},
-                {"hamster"},
-                {"bird"}};
-        System.out.println("--Original:");
-        for (String[] innerArr : w4) {
-            System.out.println(Arrays.toString(innerArr));
-        }
-        System.out.println("--Modified:");
-        FunWith2DArrays.swapFrontAndBackRows(w4);
-        for (String[] innerArr : w4) {
-            System.out.println(Arrays.toString(innerArr));
-        }
-
-        System.out.println("-------TEST 5-------");
-        String[][] w5 = {{"time"}};
-        System.out.println("--Original:");
-        for (String[] innerArr : w5) {
-            System.out.println(Arrays.toString(innerArr));
-        }
-        System.out.println("--Modified:");
-        FunWith2DArrays.swapFrontAndBackRows(w5);
-        for (String[] innerArr : w5) {
-            System.out.println(Arrays.toString(innerArr));
-        }
-
-
 
 
 
